647dea3316fac45656444be58e368a56748916fd
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-15 23:12:26 +0530

    refactor Accordion component in Landing page

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -27,20 +27,22 @@ const LandingPage = () => {
                 </Button>
             </form>
             <img src="/banner.jpeg" alt="Banner Image" className="w-full my-11 md:px-11" />
-            <Accordion type="multiple" collapsible className="w-full md:px-11">
-                <AccordionItem value="item-1">
-                    <AccordionTrigger>How does the [Website] URL shortener works?</AccordionTrigger>
-                    <AccordionContent>
+            <Accordion type="multiple" collapsible="true" className="w-full md:px-11">
+                <AccordionItem value="item-1" key="item-1">
+                    <AccordionTrigger className="text-xl">How does the [Website] URL shortener works?</AccordionTrigger>
+                    <AccordionContent className="text-lg">
                         When you enter a long URL, our system generates a shorter version of that URL. This shortened URL redirects to the original long URL when accessed.
                     </AccordionContent>
                 </AccordionItem>
-                <AccordionItem value="item-2">
-                    <AccordionTrigger>Do I need an account to use the app?</AccordionTrigger>
-                    <AccordionContent>Yes. Creating an account allows you to manage your URLs, view analytics, and customize your short URLs.</AccordionContent>
+                <AccordionItem value="item-2" key="item-2">
+                    <AccordionTrigger className="text-xl">Do I need an account to use the app?</AccordionTrigger>
+                    <AccordionContent className="text-lg">Yes. Creating an account allows you to manage your URLs, view analytics, and customize your short URLs.</AccordionContent>
                 </AccordionItem>
-                <AccordionItem value="item-3">
-                    <AccordionTrigger>What analytics are available for my shortened URLs?</AccordionTrigger>
-                    <AccordionContent>You can view the number of clicks, geolocation data of the clicks and device types (mobile/desktop) for each of your shortened URLs.</AccordionContent>
+                <AccordionItem value="item-3" key="item-3">
+                    <AccordionTrigger className="text-xl">What analytics are available for my shortened URLs?</AccordionTrigger>
+                    <AccordionContent className="text-lg">
+                        You can view the number of clicks, geolocation data of the clicks and device types (mobile/desktop) for each of your shortened URLs.
+                    </AccordionContent>
                 </AccordionItem>
             </Accordion>
         </div>
