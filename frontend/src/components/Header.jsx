5d5ab9bd026fb737d0e3ec97055200a1affbd5f1
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-15 12:28:50 +0530

    add Header Component With UI Components

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { LinkIcon, LogOut } from "lucide-react";
+
+const Header = () => {
+    const navigate = useNavigate();
+    const user = true;
+
+    return (
+        <nav className="py-4 flex justify-between items-center">
+            <Link to={"/"}>
+                <img src="/vite.svg" className="h-12" alt="Logo Image" />
+            </Link>
+            <div>
+                {!user ? (
+                    <Button onClick={() => navigate("/login")}>Login</Button>
+                ) : (
+                    <DropdownMenu>
+                        <DropdownMenuTrigger className="outline-0 overflow-hidden">
+                            <Avatar>
+                                <AvatarImage src="https://github.com/shadcn.png" />
+                                <AvatarFallback>CN</AvatarFallback>
+                            </Avatar>
+                        </DropdownMenuTrigger>
+                        <DropdownMenuContent>
+                            <DropdownMenuLabel>Username</DropdownMenuLabel>
+                            <DropdownMenuSeparator />
+                            <DropdownMenuItem>
+                                <LinkIcon /> My Links
+                            </DropdownMenuItem>
+                            <DropdownMenuItem className="text-red-500">
+                                <LogOut className="text-red-500" /> Logout
+                            </DropdownMenuItem>
+                        </DropdownMenuContent>
+                    </DropdownMenu>
+                )}
+            </div>
+        </nav>
+    );
+};
+
+export default Header;
--- frontend/src/layouts/AppLayout.jsx
+++ frontend/src/layouts/AppLayout.jsx
@@ -1,15 +1,16 @@
+import Header from "@/components/Header";
 import React from "react";
 import { Outlet } from "react-router-dom";
 
 const AppLayout = () => {
     return (
         <>
-            <h1>Header</h1>
-            <main className="min-h-screen container">
+            <main className="min-h-screen container mx-auto px-4 sm:px-6 lg:px-8">
+                <Header />
                 <Outlet />
             </main>
             <div className="p-10 text-center bg-gray-800 mt-10">
-                <p>Made With 💖 By Mohd Sameer</p>
+                <h6 className="text-xl font-bold">Made With 💖 By Mohd Sameer</h6>
             </div>
         </>
     );
