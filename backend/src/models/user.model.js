8db55fe836221ad396eade3effdf82eb20f57d9e
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-15 10:54:42 +0530

    add user model with password hashing and JWT token generation

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,83 @@
+import { Model, Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: [true, "First Name Is Required"],
+            trim: true,
+        },
+        lastName: {
+            type: String,
+            required: [true, "Last Name Is Required"],
+            trim: true,
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "Email Is Required"],
+            lowercase: true,
+            trim: true,
+        },
+        password: {
+            type: String,
+            required: [true, "Password Is Required"],
+        },
+        refreshToken: {
+            type: String,
+        },
+    },
+    { timestamps: true }
+);
+
+// Hashing The Password
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        return next();
+    } catch (error) {
+        return next(error);
+    }
+});
+
+// Checking Password Is Correct
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// Generate Access Token
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username,
+            fullName: `${this.firstName} ${this.lastName}`,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+// Generate Refresh Token
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
+
+export const User = Model("User", userSchema);
