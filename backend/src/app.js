3981d20e020746162258ea358d6e3d13c7ef1759
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-16 19:37:10 +0530

    add morgan for logging purpose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import Router from "./routes/index.routes.js";
 import cors from "cors";
+import morgan from "morgan";
 import cookieParser from "cookie-parser";
 
 const app = express();
@@ -10,6 +11,7 @@ app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(cookieParser());
+app.use(morgan("dev"));
 
 // Routes
 app.use("/api/v1/", Router);
