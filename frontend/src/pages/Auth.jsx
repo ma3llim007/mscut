8b5ad11eaf98b1b0926a9e3687fc8d0855a2ff6a
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-16 19:47:54 +0530

    if user is login then no access to auth pages

--- frontend/src/pages/Auth.jsx
+++ frontend/src/pages/Auth.jsx
@@ -1,16 +1,26 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
-import React, { lazy, Suspense } from "react";
-import { useSearchParams } from "react-router-dom";
+import UrlContext from "@/context/UrlContext";
+import React, { lazy, Suspense, useContext, useEffect } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 import { ClipLoader } from "react-spinners";
 const Login = lazy(() => import("../components/Login"));
 const Signup = lazy(() => import("../components/Signup"));
 
 const Auth = () => {
     const [searchParams] = useSearchParams();
+    const longLink = searchParams.get("createNew");
+    const navigate = useNavigate();
+    const { isAuthenticated } = useContext(UrlContext);
+
+    useEffect(() => {
+        if (isAuthenticated) {
+            navigate(`/dashboard?${longLink ? `createNew=${longLink}` : ""}`);
+        }
+    }, [isAuthenticated]);
 
     return (
         <div className="mt-10 flex flex-col items-center gap-10">
-            {searchParams.get("createNew") ? "Hold Up! Let's Login First..." : null}
+            {longLink ? "Hold Up! Let's Login First..." : null}
             <h1 className="text-5xl font-extrabold">Login / Signup</h1>
             <Tabs defaultValue="login" className="w-[400px]">
                 <TabsList className="grid w-full grid-cols-2">
