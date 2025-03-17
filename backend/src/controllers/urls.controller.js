1f0d868b70795912424bf96965d35d5d1ed949f9
ma3llim007 <mohdsameer68257@gmail.com>
2025-03-17 19:32:35 +0530

    feat: add deleteUrl functionality to remove URLs and their associated clicks; update routes to include delete endpoint

--- backend/src/controllers/urls.controller.js
+++ backend/src/controllers/urls.controller.js
@@ -1,7 +1,9 @@
+import { isValidObjectId } from "mongoose";
 import { Url } from "../models/urls.model.js";
 import { ApiError, ApiResponse, asyncHandler } from "../utils/api.utils.js";
 import { nanoid } from "nanoid";
 import { toDataURL } from "qrcode";
+import { Click } from "../models/clicks.model.js";
 
 const createUrl = asyncHandler(async (req, res) => {
     const { originalUrl, customUrl, title } = req.body;
@@ -60,4 +62,35 @@ const getUrlsByUserId = asyncHandler(async (req, res) => {
     }
 });
 
-export { getUrlsByUserId, createUrl };
+const deleteUrl = asyncHandler(async (req, res) => {
+    const { urlId } = req.params;
+
+    if (!urlId) {
+        return res.status(422).json(new ApiError(422, "Url ID is Required"));
+    }
+    if (!isValidObjectId(urlId)) {
+        return res.status(404).json(new ApiError(404, "Invalid Url ID"));
+    }
+
+    // Finding the Url
+    const url = await Url.findById(urlId);
+    if (!url) {
+        return res.status(404).json(new ApiError(404, "Url Not Found"));
+    }
+
+    // Deleting Url Relative Clicks
+    try {
+        await Click.deleteMany({ urlId });
+    } catch (error) {
+        return res.status(500).json(new ApiError(500, "Something Went Wrong! While Deleting Url Relative Clicks"));
+    }
+
+    const deleteUrl = await Url.deleteOne({ _id: urlId });
+    if (deleteUrl.deletedCount === 0) {
+        return res.status(500).json(new ApiError(500, "Something Went Wrong While Deleting The Url"));
+    }
+
+    return res.status(200).json(new ApiResponse(200, {}, "Url Delete Successfully"));
+});
+
+export { getUrlsByUserId, createUrl, deleteUrl };
--- backend/src/routes/urls.routes.js
+++ backend/src/routes/urls.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authenticateAndVerify from "../middlewares/authenticateAndVerifyUser.js";
-import { createUrl, getUrlsByUserId } from "../controllers/urls.controller.js";
+import { createUrl, deleteUrl, getUrlsByUserId } from "../controllers/urls.controller.js";
 
 const router = Router();
 
 router.route("/get-urls").get(authenticateAndVerify, getUrlsByUserId);
 router.route("/create-url").post(authenticateAndVerify, createUrl);
+router.route("/delete-url/:urlId").delete(authenticateAndVerify, deleteUrl);
 
 export default router;
